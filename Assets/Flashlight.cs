adcaf0ea800d460a021ed8edcad3032e39101490
AbhijeetGoush <530082@chrome.escg.ac.uk>
2024-04-03 16:46:39 +0100

    flashlight

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] GameObject FlashlightLight;
+    private bool flashlightOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashlightLight.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (flashlightOn == false)
+            {
+                FlashlightLight.gameObject.SetActive(true);
+                flashlightOn = true;
+            }
+            else
+            {
+                FlashlightLight.gameObject.SetActive(false);
+                flashlightOn = false;
+            }
+        }
+    }
+}
